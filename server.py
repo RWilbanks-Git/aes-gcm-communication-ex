7d55d1dee5d61a8ca3f852ebf0f06b64a61eac9f
RWilbanks-Git <rwilbanks5878@gmail.com>
2025-09-29 20:18:28 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,24 +2,22 @@
 # Name: Rachel Wilbanks
 # Language: Python
 # Program Name: server.py
-# Date: 6/15/2025
 #
 # Purpose: The purpose of this program is meant to decrypt an
 #          encrypted message delivered through a TCP connection.
 #          This is done by using AES-GCM for decryption with a
 #          key that is already shared between client and server.
-#          The program waits for a response from client
-#          (waits for a connection between the two), then receives
-#          IV and ciphertext from "client.py". After receiving it,
-#          it decrypts it.
+#          The program waits for a response from client, then 
+#          receives IV and ciphertext from "client.py". 
+#          After receiving it, it is decrypted.
 #
 #          In a nutshell, the goal is to receive and decrypt/decode
 #          text/a message sent by a client.
 #
 #
 
-import socket       #Socket handles the TCP connection (I believe)
-import os           #Used to generate random bytes (IV)
+import socket       #Handles the TCP connection
+import os           #Generate random bytes (IV)
 
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 #^For AES Encryption
@@ -56,4 +54,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print(f"Server:)     Decrypted Message is: {decryptedText.decode()}")
         except Exception as e:
             print(f"Server:)     Decryption failed: {e}")
+
 
